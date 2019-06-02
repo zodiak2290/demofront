f6ff14d65e406b5e2c94da7e5dbfad20cf737a51
alberto <albon_marvel@hotmail.com>
2019-06-01 19:00:11 -0500

    agregando componentes principales, y guard

---
+++ src/app/route/route-routing.module.ts
@@ -0,0 +1,17 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from '../components/home/home.component';
+import { LoginComponent } from '../components/login/login.component';
+import { RegisterComponent } from '../components/register/register.component';
+import { RutaGuard }  from '../validacion/ruta.guard';
+
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'home', component: HomeComponent, canActivate: [RutaGuard] },
+  { path: 'login', component: LoginComponent },
+  { path: 'registro', component: RegisterComponent },
+];
+
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
