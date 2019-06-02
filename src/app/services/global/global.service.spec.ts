5229d84e709be551b47e202d153ec0a9b66dc719
alberto <albon_marvel@hotmail.com>
2019-06-01 21:15:28 -0500

    agregando url servidor

--- src/app/services/global/global.service.spec.ts
+++ src/app/services/global/global.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { GlobalService } from './global.service';
+import { GLOBAL } from './global.service';
 
 describe('GlobalService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: GlobalService = TestBed.get(GlobalService);
+    const service: GLOBAL = TestBed.get(GLOBAL);
     expect(service).toBeTruthy();
   });
 });
--- src/app/services/global/global.service.ts
+++ src/app/services/global/global.service.ts
@@ -1,3 +1,3 @@
 export var GLOBAL = {
-  url: 'http://localhost:3789/api/'
+  url: 'https://proyectoestadisticas.herokuapp.com/api/'
 }
