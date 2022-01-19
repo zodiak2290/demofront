96621b23d4f65196fc8768600a86b576ce47aaf8
alberto <albon_marvel@hotmail.com>
2022-01-19 15:51:27 -0600

    implement firebase

--- src/app/components/timeline/timeline.component.ts
+++ src/app/components/timeline/timeline.component.ts
@@ -11,11 +11,36 @@ export class TimelineComponent implements OnInit {
   constructor(
     private empresaService: EmpresaService
   ) {
-    this.empresas = this.empresaService.getEmpresas();
-    console.log(this.empresas);
+    //this.empresas = this.empresaService.getEmpresas();
+    //console.log(this.empresas);
+
+    
   }
 
   ngOnInit(): void {
+    this.getEmpresas();
+  }
+
+  async getEmpresas(){
+    let querySnapshot = await this.empresaService.getEmpresasAsync();
+    console.log("las emrpesas")
+    console.log(querySnapshot)
+    querySnapshot.forEach((doc) => {
+      let data = doc.data();
+      console.log(data)
+      this.empresas.push(data);
+      this.empresas = this.empresas.sort(function (a, b) {
+          if (a.id > b.id) {
+            return -1;
+          }
+          if (a.id < b.id) {
+            return 1;
+          }
+          // a must be equal to b
+          return 0;
+        });
+      //this.empresas =  data;
+    }); 
   }
 
 }
