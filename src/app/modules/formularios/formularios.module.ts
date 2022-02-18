19b08fe26f51aec5ef6bb042115b55f735bc5b67
alberto <albon_marvel@hotmail.com>
2022-02-17 21:56:51 -0600

    Modularizando app

---
+++ src/app/modules/formularios/formularios.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule
+  ]
+})
+export class FormulariosModule { }
