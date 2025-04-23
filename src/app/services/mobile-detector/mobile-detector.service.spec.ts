71826bfff5876ecdfe97d4810bb3a3b3ce019a9c
alberto <albon_marvel@hotmail.com>
2025-04-22 19:18:30 -0600

    add test

--- src/app/services/mobile-detector/mobile-detector.service.spec.ts
+++ src/app/services/mobile-detector/mobile-detector.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { MobileDetectorService } from './mobile-detector.service';
+import { first } from 'rxjs';
 
 describe('MobileDetectorService', () => {
   let service: MobileDetectorService;
@@ -15,32 +16,27 @@ describe('MobileDetectorService', () => {
   });
 
   it('should detect mobile when window width is less than or equal to 768', () => {
-    spyOnProperty(window, 'innerWidth').and.returnValue(768);
-    window.dispatchEvent(new Event('resize'));
+    spyOnProperty(window, 'innerWidth', 'get').and.returnValue(768);
+    service.update();
     expect(service.isMobile).toBeTrue();
   });
 
   it('should detect non-mobile when window width is greater than 768', () => {
-    spyOnProperty(window, 'innerWidth').and.returnValue(769);
-    window.dispatchEvent(new Event('resize'));
+    spyOnProperty(window, 'innerWidth', 'get').and.returnValue(769);
+    service.update();
     expect(service.isMobile).toBeFalse();
   });
 
-  it('should emit true on isMobile$ when window width is less than or equal to 768', (done) => {
-    spyOnProperty(window, 'innerWidth').and.returnValue(768);
-    window.dispatchEvent(new Event('resize'));
-    service.isMobile$.subscribe((isMobile) => {
+  it('should emit isMobile$ observable when window is resized', (done) => {
+    spyOnProperty(window, 'innerWidth', 'get').and.returnValue(768);
+
+    const service = new MobileDetectorService();
+
+    service.isMobile$.pipe(first()).subscribe((isMobile) => {
       expect(isMobile).toBeTrue();
       done();
     });
-  });
 
-  it('should emit false on isMobile$ when window width is greater than 768', (done) => {
-    spyOnProperty(window, 'innerWidth').and.returnValue(769);
     window.dispatchEvent(new Event('resize'));
-    service.isMobile$.subscribe((isMobile) => {
-      expect(isMobile).toBeFalse();
-      done();
-    });
   });
 });
--- src/app/services/mobile-detector/mobile-detector.service.ts
+++ src/app/services/mobile-detector/mobile-detector.service.ts
@@ -1,19 +1,21 @@
 import { Injectable, NgZone } from '@angular/core';
-import { BehaviorSubject, fromEvent } from 'rxjs';
+import { BehaviorSubject, fromEvent, Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class MobileDetectorService {
   private readonly mobileSubject = new BehaviorSubject<boolean>(this.checkIsMobile());
-  public readonly isMobile$ = this.mobileSubject.asObservable();
+  public readonly isMobile$: Observable<boolean> = this.mobileSubject.asObservable();
 
   constructor() {
-    window.addEventListener('resize', () => {
-      this.mobileSubject.next(this.checkIsMobile());
-    });
+    window.addEventListener('resize', () => this.update());
   }
 
   private checkIsMobile(): boolean {
     return window.innerWidth <= 768;
+  }
+
+  public update(): void {
+    this.mobileSubject.next(this.checkIsMobile());
   }
 
   public get isMobile(): boolean {
